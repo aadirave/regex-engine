59129135d00dd4a1ad131a152d1e7e73bc3f1879
Aadi Rave <arave@purdue.edu>
2025-04-24 15:42:00 -0400

    added todo

--- lib/ast.mli
+++ lib/ast.mli
@@ -4,3 +4,16 @@ type regex_t =
 | Concat of regex_t * regex_t
 | Choice of regex_t * regex_t
 | Star of regex_t
+
+(*
+TODO:
+  Add 
+  | Maybe of regex_t
+  | Plus of regex_t
+  | Maybe of regex_t
+  
+  Non trivial
+  | AnyChar of regex_t
+  | Charset
+  | Negated Charset
+*)
